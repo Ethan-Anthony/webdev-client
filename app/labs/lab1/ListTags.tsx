853e9b73b1536580051956822cbf07d292623af5
Ethan-Anthony <anthony.e@northeastern.edu>
2026-09-09 20:26:22 -0400

    A1 lab init

---
+++ app/labs/lab1/ListTags.tsx
@@ -0,0 +1,55 @@
+export default function ListTags() {
+    return (
+      <div id="wd-lists">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        <ol id="wd-pancakes">
+          <li>Mix dry ingredients.</li>
+          <li>Add wet ingredients.</li>
+          <li>Stir to combine.</li>
+          <li>Heat a skillet or griddle.</li>
+          <li>Pour batter onto the skillet.</li>
+          <li>Cook until bubbly on top.</li>
+          <li>Flip and cook the other side.</li>
+          <li>Serve and enjoy!</li>
+        </ol>
+        <h5>Unordered List Tag</h5>
+        My favorite books (in no particular order)
+        <ul id="wd-my-books">
+            <li>Dune</li>
+            <li>Lord of the Rings</li>
+            <li>Ender&apos;s Game</li>
+            <li>Red Mars</li>
+            <li>The Forever War</li>
+        </ul>
+        <h5>HTML Tags From This Chapter</h5>
+        Tags covered so far:
+        <ul id="wd-ai-html-tags">
+            <li>h1 &ndash; the largest heading, used for the main title</li>
+            <li>p &ndash; wraps a paragraph and adds vertical spacing</li>
+            <li>ol &ndash; an ordered list, numbered in sequence</li>
+            <li>ul &ndash; an unordered list, marked with bullets</li>
+            <li>li &ndash; a single item inside an ordered or unordered list</li>
+            <li>table &ndash; arranges content into rows and columns</li>
+            <li>span &ndash; groups inline text without starting a new line</li>
+        </ul>
+        <h5>Ethan's Recipe</h5>
+        How to make cereal:
+        <ol id="wd-your-favorite-recipe">
+            <li>Take out ingredients (bowl, cereal of choice, and milk)</li>
+            <li>Pour the cereal into the bowl</li>
+            <li>Pour the milk into the bowl</li>
+            <li>Enjoy!</li>
+        </ol>
+        <h5>Ethan's Favorite Books</h5>
+        Here are my favorite books (in no particular order)
+        <ul id="wd-your-books">
+            <li>Red Rising</li>
+            <li>Mistborn</li>
+            <li>The Will of the Many</li>
+            <li>The Way of Kings</li>
+        </ul>
+      </div>
+    );
+  }
