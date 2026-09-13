a4b41876d590439b367e6fe9eccbab0e19874285
Ethan-Anthony <anthony.e@northeastern.edu>
2026-09-13 17:43:58 -0400

    Lab fix

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -6,9 +6,9 @@ export default function TOC () {
             <h4>Ethan Anthony</h4>
             <ul>
                 <li><Link href="/">Home</Link></li>
-                <li><Link href="/labs/lab1">Lab 1</Link></li>
-                <li><Link href="/labs/lab2">Lab 2</Link></li>
-                <li><Link href="/labs/lab3">Lab 3</Link></li>
+                <li><Link href="/labs/lab1" id="wd-lab1-link">Lab 1</Link></li>
+                <li><Link href="/labs/lab2" id="wd-lab2-link">Lab 2</Link></li>
+                <li><Link href="/labs/lab3" id="wd-lab3-link">Lab 3</Link></li>
                 <li><Link href="/book/ch1" id="wd-toc-book-link">Chapter 1</Link></li>
                 <li><Link href="/kambaz">Kambaz</Link></li>
             </ul>
